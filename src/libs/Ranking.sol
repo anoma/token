6885d742b3e7d293d6d5f59007f70fa557f80cf9
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-04-25 09:58:39 +0200

    refactor: improved naming

--- src/libs/Ranking.sol
+++ src/libs/Ranking.sol
@@ -43,50 +43,65 @@ library Ranking {
     /// @notice Assigns the highest rank to a proposed implementation.
     /// @param $ The storage containing the proposed upgrades.
     /// @param proposedImpl The proposed implementation to assign the highest rank to.
-    function assignHighestRank(ProposedUpgrades storage $, address proposedImpl) internal {
-        $.ballots[proposedImpl].exists = true;
-        $.ballots[proposedImpl].rank = $.implCount;
+    function assignLowestRank(ProposedUpgrades storage $, address proposedImpl) internal {
+        Ballot storage ballot = $.ballots[proposedImpl];
+        ballot.exists = true;
+        ballot.rank = $.implCount;
 
         // Set the highest rank.
-        uint64 highestRank = $.implCount;
-        $.ranking[highestRank] = proposedImpl;
+        $.ranking[$.implCount] = proposedImpl;
         ++$.implCount;
     }
 
-    /// @notice Updates the rank of the proposed implementation and those being ranked lower (better).
+    /// @notice Bubble the proposed implementation up in the ranking.
     /// @param $ The storage containing the ballots for proposed upgrades.
     /// @param proposedImpl The proposed implementation.
-    function updateLowerRanked(ProposedUpgrades storage $, address proposedImpl) internal {
-        uint64 rank = $.ballots[proposedImpl].rank;
-        uint256 votes = $.ballots[proposedImpl].totalVotes;
+    function bubbleUp(ProposedUpgrades storage $, address proposedImpl) internal {
+        Ballot storage ballot = $.ballots[proposedImpl];
+        uint64 rank = ballot.rank;
+        uint256 votes = ballot.totalVotes;
 
-        while (rank > 0) {
-            uint64 lowerRank = rank - 1;
-            (address lowerImpl, uint256 lowerVotes) = _getImplAndVotes($, lowerRank);
+        uint64 bestRank = 0;
 
-            if (votes <= lowerVotes) break;
+        while (rank > bestRank) {
+            uint64 nextBetterRank;
+            unchecked {
+                nextBetterRank = rank - 1;
+            }
 
-            _swapRank({$: $, implA: proposedImpl, rankA: rank, implB: lowerImpl, rankB: lowerRank});
-            rank = lowerRank;
+            address nextBetterImpl = $.ranking[nextBetterRank];
+            uint256 nextBetterVotes = $.ballots[nextBetterImpl].totalVotes;
+
+            if (votes <= nextBetterVotes) break;
+
+            _swapRank({$: $, implA: proposedImpl, rankA: rank, implB: nextBetterImpl, rankB: nextBetterRank});
+            rank = nextBetterRank;
         }
     }
 
-    /// @notice Updates the rank of the proposed implementation and those being ranked higher (worse).
+    /// @notice Bubble the proposed implementation down in the ranking.
     /// @param $ The storage containing the ballots for proposed upgrades.
     /// @param proposedImpl The proposed implementation.
-    function updateHigherRanked(ProposedUpgrades storage $, address proposedImpl) internal {
-        uint64 rank = $.ballots[proposedImpl].rank;
-        uint256 votes = $.ballots[proposedImpl].totalVotes;
-        uint64 maxRank = $.implCount - 1;
+    function bubbleDown(ProposedUpgrades storage $, address proposedImpl) internal {
+        Ballot storage ballot = $.ballots[proposedImpl];
+        uint64 rank = ballot.rank;
+        uint256 votes = ballot.totalVotes;
 
-        while (rank < maxRank) {
-            uint64 higherRank = rank + 1;
-            (address higherImpl, uint256 higherVotes) = _getImplAndVotes($, higherRank);
+        uint64 worstRank = $.implCount - 1;
 
-            if (votes > higherVotes) break;
+        while (rank < worstRank) {
+            uint64 nextWorseRank;
+            unchecked {
+                nextWorseRank = rank + 1;
+            }
 
-            _swapRank({$: $, implA: proposedImpl, rankA: rank, implB: higherImpl, rankB: higherRank});
-            rank = higherRank;
+            address nextWorseImpl = $.ranking[nextWorseRank];
+            uint256 nextWorseVotes = $.ballots[nextWorseImpl].totalVotes;
+
+            if (votes > nextWorseVotes) break;
+
+            _swapRank({$: $, implA: proposedImpl, rankA: rank, implB: nextWorseImpl, rankB: nextWorseRank});
+            rank = nextWorseRank;
         }
     }
 
@@ -102,19 +117,5 @@ library Ranking {
 
         $.ballots[implA].rank = rankB;
         $.ballots[implB].rank = rankA;
-    }
-
-    /// @notice Returns the implementation and associated votes for a specific rank.
-    /// @param $ The storage containing the ballots for proposed upgrades.
-    /// @param rank The rank to return the implementation and votes for.
-    /// @return impl The implementation for the specific rank.
-    /// @return votes The votes for the specific rank.
-    function _getImplAndVotes(ProposedUpgrades storage $, uint64 rank)
-        private
-        view
-        returns (address impl, uint256 votes)
-    {
-        impl = $.ranking[rank];
-        votes = $.ballots[impl].totalVotes;
     }
 }
