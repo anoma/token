3bfaf0bf3384b6528bef039a02b39ad281a93d35
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-05-06 21:52:54 +0200

    refactor: renaming

--- test/mocks/Persons.m.sol
+++ test/mocks/Persons.m.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.27;
 
 contract MockPersons {
-    string[16] public names = [
+    string[16] internal _names = [
         "Alice",
         "Bob",
         "Carol",
@@ -21,31 +21,31 @@ contract MockPersons {
         "Paul"
     ];
 
-    address[16] public addresses;
-    mapping(string name => address person) public persons;
-    mapping(string name => uint256 personId) public personIds;
+    address[16] internal _addresses;
+    mapping(string name => address person) internal _persons;
+    mapping(string name => uint256 personId) internal _personIds;
 
     constructor() {
-        for (uint256 i = 0; i < names.length; ++i) {
-            addresses[i] = address(uint160(i + 1));
-            persons[names[i]] = addresses[i];
-            personIds[names[i]] = i;
+        for (uint256 i = 0; i < _names.length; ++i) {
+            _addresses[i] = address(uint160(i + 1));
+            _persons[_names[i]] = _addresses[i];
+            _personIds[_names[i]] = i;
         }
     }
 
-    function person(string memory name) public view returns (address addr) {
-        addr = persons[name];
+    function person(string memory name) internal view returns (address addr) {
+        addr = _persons[name];
     }
 
-    function person(uint256 id) public view returns (address addr) {
-        addr = addresses[id];
+    function person(uint256 id) internal view returns (address addr) {
+        addr = _addresses[id];
     }
 
-    function personId(string memory name) public view returns (uint256 id) {
-        id = personIds[name];
+    function personId(string memory name) internal view returns (uint256 id) {
+        id = _personIds[name];
     }
 
-    function personAddrAndId(string memory name) public view returns (address addr, uint256 id) {
+    function personAddrAndId(string memory name) internal view returns (address addr, uint256 id) {
         addr = person(name);
         id = personId(name);
     }
