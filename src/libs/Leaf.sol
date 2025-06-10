b92dc508b2bff6dacc2a2887d73edba482f881de
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-06-10 20:20:59 +0200

    revert: remove redundant file

--- src/libs/Leaf.sol
+++
@@ -1,8 +0,0 @@
-// SPDX-License-Identifier: AGPL-3.0-or-later
-pragma solidity ^0.8.30;
-
-library Leaf {
-    function hash(uint256 index, address to, uint256 value, bool locked) internal pure returns (bytes32 leafHash) {
-        leafHash = keccak256(abi.encode(index, to, value, locked));
-    }
-}
