4899fe7b9b06af62e4d0685bfded6cc1cdd5d54d
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-07-09 09:09:46 +0200

    feat: add tests

--- test/XanV1.criteria.t.sol
+++ test/XanV1.criteria.t.sol
@@ -18,83 +18,67 @@ contract MockXanV1CriteriaTest is Test {
     using SafeERC20 for MockXanV1;
 
     address internal constant _COUNCIL = address(uint160(1));
-    address internal constant _IMPL_A = address(uint160(2));
-    address internal constant _IMPL_B = address(uint160(3));
+    address internal constant _NEW_IMPL = address(uint160(2));
 
-    address internal _defaultSender;
-    address internal _alice;
-    address internal _bob;
-
+    address internal _tokenHolder;
     MockXanV1 internal _xanProxyMock;
 
     function setUp() public {
-        (, _defaultSender,) = vm.readCallers();
+        (, _tokenHolder,) = vm.readCallers();
 
-        // Deploy proxy and mint tokens for the `_defaultSender`.
-        vm.prank(_defaultSender);
+        // Deploy proxy and mint tokens for the `_tokenHolder`.
+        vm.prank(_tokenHolder);
         _xanProxyMock = MockXanV1(
             Upgrades.deployUUPSProxy({
                 contractName: "XanV1.m.sol:MockXanV1",
-                initializerData: abi.encodeCall(XanV1.initializeV1, (_defaultSender, _COUNCIL))
+                initializerData: abi.encodeCall(XanV1.initializeV1, (_tokenHolder, _COUNCIL))
             })
         );
-
-        _alice = vm.randomAddress();
-        _bob = vm.randomAddress();
     }
 
     function test_isQuorumAndMinLockedSupplyReached_returns_false_if_the_quorum_threshold_is_not_exceeded() public {
-        uint256 halfOfSupply = _xanProxyMock.totalSupply() / 2;
+        vm.startPrank(_tokenHolder);
+        _xanProxyMock.lock(Parameters.MIN_LOCKED_SUPPLY / 2);
+        _xanProxyMock.castVote(_NEW_IMPL);
 
-        vm.startPrank(_defaultSender);
-        _xanProxyMock.safeTransfer(_alice, halfOfSupply);
-        _xanProxyMock.safeTransfer(_bob, halfOfSupply);
-        vm.stopPrank();
+        uint256 votum = _xanProxyMock.votum(_tokenHolder, _NEW_IMPL);
+        _xanProxyMock.lock(Parameters.MIN_LOCKED_SUPPLY - votum);
 
-        assertEq(_xanProxyMock.balanceOf(_defaultSender), 0);
-        assertEq(_xanProxyMock.balanceOf(_alice), halfOfSupply);
-        assertEq(_xanProxyMock.balanceOf(_bob), halfOfSupply);
-
-        vm.startPrank(_alice);
-        _xanProxyMock.lock(halfOfSupply);
-        _xanProxyMock.castVote(_IMPL_A);
-        vm.stopPrank();
-
-        vm.prank(_bob);
-        _xanProxyMock.lock(halfOfSupply);
-        vm.stopPrank();
-
-        assertEq(_xanProxyMock.isQuorumAndMinLockedSupplyReached(_IMPL_A), false);
+        assertEq(_xanProxyMock.lockedSupply(), Parameters.MIN_LOCKED_SUPPLY);
+        assertEq(_xanProxyMock.isQuorumAndMinLockedSupplyReached(_NEW_IMPL), false);
     }
 
     function test_isQuorumAndMinLockedSupplyReached_returns_true_if_the_quorum_threshold_is_exceeded_by_one_vote()
         public
     {
-        uint256 halfOfSupply = _xanProxyMock.totalSupply() / 2;
+        vm.startPrank(_tokenHolder);
+        _xanProxyMock.lock(Parameters.MIN_LOCKED_SUPPLY / 2 + 1);
+        _xanProxyMock.castVote(_NEW_IMPL);
 
-        vm.startPrank(_defaultSender);
-        _xanProxyMock.safeTransfer(_alice, halfOfSupply + 1);
-        _xanProxyMock.safeTransfer(_bob, halfOfSupply - 1);
-        vm.stopPrank();
+        uint256 votum = _xanProxyMock.votum(_tokenHolder, _NEW_IMPL);
+        _xanProxyMock.lock(Parameters.MIN_LOCKED_SUPPLY - votum);
 
-        assertEq(_xanProxyMock.balanceOf(_defaultSender), 0);
-        assertEq(_xanProxyMock.balanceOf(_alice), halfOfSupply + 1);
-        assertEq(_xanProxyMock.balanceOf(_bob), halfOfSupply - 1);
-
-        vm.startPrank(_alice);
-        _xanProxyMock.lock(halfOfSupply + 1);
-        _xanProxyMock.castVote(_IMPL_A);
-        vm.stopPrank();
-
-        vm.startPrank(_bob);
-        _xanProxyMock.lock(halfOfSupply - 1);
-        vm.stopPrank();
-        assertEq(_xanProxyMock.lockedSupply(), _xanProxyMock.totalSupply());
-
-        assertEq(_xanProxyMock.isQuorumAndMinLockedSupplyReached(_IMPL_A), true);
+        assertEq(_xanProxyMock.lockedSupply(), Parameters.MIN_LOCKED_SUPPLY);
+        assertEq(_xanProxyMock.isQuorumAndMinLockedSupplyReached(_NEW_IMPL), true);
     }
 
-    function test_authorizeUpgrade_requires_the_threshold_to_be_passed_with_one_more_vote_than_the_limit() public {
-        revert("TODO");
+    function test_isQuorumAndMinLockedSupplyReached_returns_false_if_the_min_locked_supply_is_not_met_by_one_vote()
+        public
+    {
+        vm.startPrank(_tokenHolder);
+        _xanProxyMock.lock(Parameters.MIN_LOCKED_SUPPLY - 1);
+        _xanProxyMock.castVote(_NEW_IMPL);
+
+        assertLt(_xanProxyMock.lockedSupply(), Parameters.MIN_LOCKED_SUPPLY);
+        assertEq(_xanProxyMock.isQuorumAndMinLockedSupplyReached(_NEW_IMPL), false);
+    }
+
+    function test_isQuorumAndMinLockedSupplyReached_returns_true_if_the_min_locked_supply_is_exactly_met() public {
+        vm.startPrank(_tokenHolder);
+        _xanProxyMock.lock(Parameters.MIN_LOCKED_SUPPLY);
+        _xanProxyMock.castVote(_NEW_IMPL);
+
+        assertEq(_xanProxyMock.lockedSupply(), Parameters.MIN_LOCKED_SUPPLY);
+        assertEq(_xanProxyMock.isQuorumAndMinLockedSupplyReached(_NEW_IMPL), true);
     }
 }
