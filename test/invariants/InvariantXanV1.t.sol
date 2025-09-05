4ce955bce246d31aad653db5b23beafdcb93da7e
Michael Heuer <michael.heuer@rwth-aachen.de>
2025-09-05 23:00:19 +0200

    style: apply style guide and fix linter warnings

--- test/invariants/InvariantXanV1.t.sol
+++ test/invariants/InvariantXanV1.t.sol
@@ -1,27 +1,29 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.30;
 
-import {Test, StdInvariant} from "forge-std/Test.sol";
 import {ERC1967Proxy} from "@openzeppelin/contracts/proxy/ERC1967/ERC1967Proxy.sol";
-import {XanV1} from "src/XanV1.sol";
+
+import {Test, StdInvariant} from "forge-std/Test.sol";
+
+import {XanV1} from "../../src/XanV1.sol";
 import {XanV1Handler} from "./HandlerXanV1.t.sol";
 
 contract XanV1Invariants is StdInvariant, Test {
     XanV1 public token;
     XanV1Handler public handler;
 
-    address internal alice = makeAddr("alice");
-    address internal council = makeAddr("council");
+    address internal _alice = makeAddr("alice");
+    address internal _council = makeAddr("council");
 
     function setUp() public {
         // Deploy implementation
         XanV1 impl = new XanV1();
         // Deploy proxy with initializer
-        bytes memory init = abi.encodeWithSelector(XanV1.initializeV1.selector, alice, council);
+        bytes memory init = abi.encodeWithSelector(XanV1.initializeV1.selector, _alice, _council);
         ERC1967Proxy proxy = new ERC1967Proxy(address(impl), init);
         token = XanV1(payable(address(proxy)));
 
-        handler = new XanV1Handler(token, alice);
+        handler = new XanV1Handler(token, _alice);
 
         // Register the handler for invariant fuzzing
         targetContract(address(handler));
@@ -36,7 +38,7 @@ contract XanV1Invariants is StdInvariant, Test {
         address[] memory actors = handler.getActors();
         uint256 sumLocked = 0;
 
-        for (uint256 i = 0; i < actors.length; i++) {
+        for (uint256 i = 0; i < actors.length; ++i) {
             uint256 balance = token.balanceOf(actors[i]);
             uint256 locked = token.lockedBalanceOf(actors[i]);
             uint256 unlocked = token.unlockedBalanceOf(actors[i]);
@@ -55,7 +57,7 @@ contract XanV1Invariants is StdInvariant, Test {
     function invariant_lockedBalanceMonotonicity() public view {
         address[] memory actors = handler.getActors();
 
-        for (uint256 i = 0; i < actors.length; i++) {
+        for (uint256 i = 0; i < actors.length; ++i) {
             address account = actors[i];
             uint256 currentLocked = token.lockedBalanceOf(account);
             uint256 previousLocked = handler.getPreviousLockedBalance(account);
@@ -70,11 +72,11 @@ contract XanV1Invariants is StdInvariant, Test {
         address[] memory actors = handler.getActors();
         address[5] memory validImpls = handler.getValidImpls();
 
-        for (uint256 i = 0; i < actors.length; i++) {
+        for (uint256 i = 0; i < actors.length; ++i) {
             address voter = actors[i];
 
             // Check all valid implementation addresses to verify monotonicity
-            for (uint256 j = 0; j < validImpls.length; j++) {
+            for (uint256 j = 0; j < validImpls.length; ++j) {
                 address impl = validImpls[j];
                 uint256 currentVotes = token.getVotes(voter, impl);
                 uint256 previousVotes = handler.getPreviousVotes(voter, impl);
@@ -89,9 +91,9 @@ contract XanV1Invariants is StdInvariant, Test {
         (address vImpl, uint48 vEnd) = token.scheduledVoterBodyUpgrade();
         (address cImpl, uint48 cEnd) = token.scheduledCouncilUpgrade();
         bool voterScheduled = (vImpl != address(0) && vEnd != 0);
-        bool councilScheduled = (cImpl != address(0) && cEnd != 0);
+        bool _councilScheduled = (cImpl != address(0) && cEnd != 0);
 
-        assertTrue(!(voterScheduled && councilScheduled), "both voter and council scheduled");
+        assertTrue(!(voterScheduled && _councilScheduled), "both voter and _council scheduled");
     }
 
     //PS-9 (upgrade cancellation token persistence) is implicitly proved by invariant tests above
