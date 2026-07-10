357d179eb3ef473e169f87ffe52e9c7899326a82
Michael Heuer <michael.heuer@rwth-aachen.de>
2026-07-10 17:06:39 +0200

    doc: remove redundant NatSpec in tests

--- test/script/UpgradeXanV1.t.sol
+++ test/script/UpgradeXanV1.t.sol
@@ -7,8 +7,6 @@ import {Test} from "forge-std/Test.sol";
 import {UpgradeXanV1} from "../../script/UpgradeXanV1.s.sol";
 import {XanV1} from "../../src/XanV1.sol";
 
-/// @notice Covers the pre-flight guards `UpgradeXanV1` checks before broadcasting the upgrade. The happy path is
-/// exercised by the upgrade integration and e2e tests.
 contract UpgradeXanV1Test is Test {
     address internal immutable _COUNCIL = makeAddr("council");
 
@@ -22,13 +20,11 @@ contract UpgradeXanV1Test is Test {
         _script = new UpgradeXanV1();
     }
 
-    /// @notice `run` reverts when no council upgrade has been scheduled.
     function test_run_reverts_if_no_upgrade_is_scheduled() public {
         vm.expectRevert(UpgradeXanV1.ZeroImplementationV2NotAllowed.selector, address(_script));
         _script.run({proxy: _proxy});
     }
 
-    /// @notice `run` reverts when the scheduled council delay has not yet elapsed.
     function test_run_reverts_before_the_delay_has_elapsed() public {
         vm.prank(_COUNCIL);
         XanV1(_proxy).scheduleCouncilUpgrade({impl: makeAddr("implV2")});
